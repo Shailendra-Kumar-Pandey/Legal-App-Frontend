d631becc3fe711154c629f727d44878f838d2529
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-03-17 00:34:06 +0530

    practice React tailwindcss

--- src/Component/Registration.jsx
+++ src/Component/Registration.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function Registration() {
+
+    const { register, handleSubmit, formState: {errors} } = useForm();
+
+
+    function submit(data){
+        console.log(data)
+    }
+
   return (
     <>
         <div className="w-full h-screen bg-gray-300 flex">
@@ -11,29 +20,28 @@ function Registration() {
               <h1 className="text-gray-900 font-bold text-center">LegalDesk</h1>
             </div>
 
-              <h1 className="text-2xl font-serif font-bold pt-2 text-gray-900">Welcome Back</h1>
-              <p className="text-sm text-gray-400">Sign in to your account</p>
-
-              <div className="w-full mt-3 mb-3 rounded-2xl">
-                {
-                  role.map((ele, index)=>{
-                    return(
-                      
-                      <button key={index} className={ defaultRole === ele ? `w-1/3 rounded-2xl pt-1 pb-1 text-gray-300 text-sm
-                       bg-gray-900 cursor-pointer transition-all` : `w-1/3 rounded-2xl pt-1 pb-1 text-gray-400 text-sm
-                       hover:bg-gray-900 hover:text-gray-300 focus:text-gray-200 focus:bg-gray-900
-                        cursor-pointer transition-all`} onClick={()=>{setRole(ele)}}>{ele}</button>
-                    )
-                  })
-                }
-              </div>
+              <h1 className="text-2xl font-serif font-bold pt-2 text-gray-900">Welcome</h1>
+              <p className="text-sm text-gray-400">All Field Requierd in Create Account</p>
 
               <form onSubmit={handleSubmit(submit)}>
+                <label htmlFor="name" className="text-l">Full Name</label>
+                <input type="name" id="name" placeholder="Rahul Singh" className="w-full p-1 rounded-sm border-2 border-gray-200 outline-gray-900"
+                {...register('name', {required:true})}/>
+
+                {errors.name && <span className="text-red-600">Name is Required<br/></span> }
+
+                <label htmlFor="phone" className="text-l">Mobile Number</label>
+                <input type="phone" id="phone" placeholder="9988123456" className="w-full p-1 rounded-sm border-2 border-gray-200 outline-gray-900"
+                {...register('phone', {required:true})}/>
+
+                {errors.phone && <span className="text-red-600">Mobile Number is Required<br/></span> }
+
                 <label htmlFor="email" className="text-l">Email</label>
                 <input type="email" id="email" placeholder=" ✉  you@example.com" className="w-full p-1 rounded-sm border-2 border-gray-200 outline-gray-900"
                 {...register('email', {required:true})}/>
 
                 {errors.email && <span className="text-red-600">Email is Required<br/></span> }
+
 
                 <label htmlFor="password" className="text-l">Password</label>
                 <input type="password" id="password" placeholder="🔒 ********"  className="w-full p-1 rounded-sm border-gray-200 border-2 outline-gray-900" 
@@ -41,10 +49,16 @@ function Registration() {
 
                 {errors.password && <span className="text-red-600">Password is Required <br/></span>}
 
-                <button className="w-full bg-blue-600 rounded-md mt-2 p-1 text-sm text-gray-300 hover:bg-sky-700 hover:text-white cursor-pointer" type="submit">Sign In</button>
+                <label htmlFor="password" className="text-l">Confirm Password</label>
+                <input type="password" id="password" placeholder="🔒 ********"  className="w-full p-1 rounded-sm border-gray-200 border-2 outline-gray-900" 
+                {...register('password', {required:true})}/>
+
+                {errors.password && <span className="text-red-600">Password is Required <br/></span>}
+
+                <button className="w-full bg-blue-600 rounded-md mt-2 p-1 text-sm text-gray-300 hover:bg-sky-700 hover:text-white cursor-pointer" type="submit">Sign Up</button>
               </form>
 
-              <p className="text-sm text-gray-400 mt-5">Don't have an account? <span className="text-blue-600 hover:text-blue-700 hover:font-bold cursor-pointer" onClick={()=>{navigator('/registration')}}> Register </span> </p>
+              
 
           </div>
         </div>
