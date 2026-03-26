796d4b507a2961b8a92532ba98dc61405bfbf990
Shailendra Pandey <shailendrakr2010@gmail.com>
2026-03-27 00:55:57 +0530

    add model

--- src/Component/Nested Components/AdminTable.jsx
+++ src/Component/Nested Components/AdminTable.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { toast } from 'react-toastify';
 
 
 
@@ -7,9 +8,21 @@ const AdminTable = ({lawyerData}) => {
   const getStatusColor = (status) => {
     if (status === "APPROVED") return "bg-green-100 text-green-600";
     if (status === "PENDING") return "bg-yellow-100 text-yellow-600";
-    if (status === "BLOCKED") return "bg-red-100 text-red-600";
+    if (status === "BLOCKED") return "bg-red-200 text-red-600";
+    if (status === "RETURNED") return "bg-orange-100 text-orange-600";
+    if (status === "REJECTED") return "bg-red-100 text-red-600";
   };
   console.log(lawyerData)
+
+  async function changeStatus(status){
+    try {
+      console.log(status)
+    } catch (error) {
+      toast.error("Server Error!")
+    }
+  }
+
+
   return (
     <div className="flex  bg-gray-100">
 
@@ -75,57 +88,109 @@ const AdminTable = ({lawyerData}) => {
 
                       {/* Modal */}
                       {model && (
-                        <div className="fixed inset-0 flex items-center justify-center bg-black/25">
-
-                          <div className="bg-gray-50 p-6 rounded-lg shadow-lg w-1/2 h-1/2">
-                            <button className='text-gray-50 bg-gray-900 rounded-[50%] p-2 absolute right-90 hover:bg-red-500 cursor-pointer' onClick={() => setModel(false)}>X</button>
+                        <div className=" fixed inset-0 flex items-center justify-center bg-black/25">
+                          
+                          <div className="bg-gray-50 p-6 rounded-lg shadow-lg w-[75%] h-[60%] ">
+                            <button className='text-gray-50 bg-gray-900 rounded-[50%] p-2 absolute right-45 hover:bg-red-500 cursor-pointer' onClick={() => setModel(false)}>X</button>
                             <h2 className="text-xl font-bold mb-4 text-center text-gray-900">Lawyers Detailes</h2>
                           
                           <div className='flex flex-wrap gap-12'>
                             <div>
                               <h3 className='text-sm font-semibold text-gray-900 '>Name</h3>
-                              <p className='text-xm text-gray-700'>Shailedra Kumar Pandey</p>
+                              <p className='text-xm text-gray-700'>{lawyer.userId.name}</p>
+                            </div>
+                            <div> 
+                              <h3  className='text-sm font-semibold text-gray-900'>Email</h3>
+                              <p className='text-xm text-gray-700'>{lawyer.userId.email}</p>
+                            </div>
+                            <div> 
+                              <h3  className='text-sm font-semibold text-gray-900'>Phone</h3>
+                              <p className='text-xm text-gray-700'>{lawyer.userId.Phone}</p>
+                            </div>
+                            <div>
+                              <h3 className='text-sm font-semibold text-gray-900' >barCouncil Id</h3>
+                              <p className='text-xm text-gray-700'>{lawyer.barCouncilId}</p>
                             </div>
                             <div> 
                               <h3  className='text-sm font-semibold text-gray-900'>Specialization</h3>
-                              <p className='text-xm text-gray-700'>Criminal</p>
+                              <p className='text-xm text-gray-700'>{lawyer.lawyerType}</p>
                             </div>
                             <div>
                               <h3 className='text-sm font-semibold text-gray-900'>Experience</h3>
-                              <p className='text-xm text-gray-700'>10 Years</p>
+                              <p className='text-xm text-gray-700'>{lawyer.experienceYears} Years</p>
                             </div>
                             <div>
-                              <h3 className='text-sm font-semibold text-gray-900'>Win Ratio</h3>
-                              <p className='text-xm text-gray-700'>100%</p>
+                              <h3 className='text-sm font-semibold text-gray-900' >Degree</h3>
+                              <p className='text-xm text-gray-700'>{lawyer.degree}</p>
+                            </div>
+                            <div>
+                              <h3 className='text-sm font-semibold text-gray-900'>Win Case</h3>
+                              <p className='text-xm text-gray-700'>{lawyer.wonCases} Case</p>
+                            </div>
+                            <div>
+                              <h3 className='text-sm font-semibold text-gray-900' >Lost Case</h3>
+                              <p className='text-xm text-gray-700'>{lawyer.lostCases}</p>
                             </div>
                             <div>
                               <h3 className='text-sm font-semibold text-gray-900' >Status</h3>
-                              <p className='text-xm text-gray-700'>Pending</p>
+                              <p className='text-xm text-gray-700'>{lawyer.status}</p>
                             </div>
+                            
+                            <div>
+                              <h3 className='text-sm font-semibold text-gray-900' >Minimam Fees</h3>
+                              <p className='text-xm text-gray-700'>₹ {lawyer.feeMin}</p>
+                            </div>
+                            <div>
+                              <h3 className='text-sm font-semibold text-gray-900' >Maximam Fees</h3>
+                              <p className='text-xm text-gray-700'>₹ {lawyer.feeMax}</p>
+                            </div>
+                            
                           </div>
                           
                           <div className='flex p-5 gap-10 justify-around items-center'>
-                           <select name="" id="" className='text-sm font-semibold text-gray-900'>
-                            <option value="">Select Status</option>
-                            <option value="Approved">Approved</option>
-                            <option value="Blocked">Blocked</option>
-                            <option value="Pending">Pending</option>
-                            <option value="Rejected">Rejected</option>
-                            </select> 
+                           
                             <div>
                               <label htmlFor="" className='text-sm font-semibold text-gray-900'>Remark</label><br />
-                              <textarea className='bg-gray-200  text-gray-900 p-2 rounded' name="" id="" cols="50" rows='3' placeholder='Please Enter Remark...'>
+                              <textarea className='bg-gray-200  text-gray-900 p-2 rounded' name="" id="" cols="50" rows='3' placeholder='Please Enter Remark...' onKeyUp={(e)=>{}}>
 
                               </textarea>
                             </div>
+                            <div className='flex gap-2 flex-row'>
+
+                           
+                            <button
+                              value="RETURNED"
+                              onClick={() => setModel(false)}
+                              className="px-4 py-2 bg-gray-900 hover:bg-yellow-400 cursor-pointer text-white rounded"
+                            >
+                              Returned
+                            </button>
+                            <button
+                              value= "APPROVED"
+                              onClick={() => setModel(false)}
+                              className="px-4 py-2 bg-gray-900 hover:bg-green-600 cursor-pointer text-white rounded"
+                            >
+                              Approved
+                            </button>
+                            <button
+                              value="REJECTED"
+                              onClick={() => setModel(false)}
+                              className="px-4 py-2 bg-gray-900 hover:bg-orange-600 cursor-pointer text-white rounded"
+                            >
+                              Rejected
+                            </button>
+                            <button
+                              value="BLOCKED"
+                              onClick={() => setModel(false)}
+                              className="px-4 py-2 bg-gray-900 hover:bg-red-600 cursor-pointer text-white rounded"
+                            >
+                              Blocked
+                            </button>
+                          </div>
                           </div>
 
-                            <button
-                              onClick={() => setModel(false)}
-                              className="px-4 py-2 w-full bg-gray-900 cursor-pointer text-white rounded"
-                            >
-                              Status Change
-                            </button>
+                          
+
 
                           </div>
                         </div>
